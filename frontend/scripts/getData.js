100acbba6758a9c25b4eca3324b99bf21d8c1e0d
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-10-16 13:43:04 +0700

    load products

--- frontend/scripts/getData.js
+++ frontend/scripts/getData.js
@@ -1,26 +1,82 @@
-$(document).ready(() => {
-  console.log("Ready event");
-  $.ajax({
-    type:"GET",
-    dataType: "json",
-    crossDomain:true,
-    url: "https://localhost:7136/items",
+const url = "https://localhost:7136";
+
+const getSingleItem = async (id) => {
+  itemUrl = url + `/items/${id}`;
+  const resp = await fetch(url, {
+    method: "GET",
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Headers":"*"
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    // data: {
-    //   //   zipcode: 97201
-    // },
-    success: function (result) {
-      //   $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
-      console.log(result);
-    },
-      error: function (xhr, status) {
-      alert("error");
-  }
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(id), // body data type must match "Content-Type" header
   });
-});
+  const data = await resp.json();
+  console.log(data);
+};
+
+const getData = async () => {
+  itemUrl = url + `/items`;
+  const resp = await fetch(itemUrl, {
+    method: "GET",
+    // mode: "cors", // no-cors, *cors, same-origin
+    // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  const data = await resp.json();
+  data.map((item) => {
+    document.querySelector(".itemsList").insertAdjacentHTML('beforeend',`<div class ="items"><a href="">${item.pName}</a>
+    <img src = "../data/Crawled Images/${item.pid}_1.png"/>
+    <h3>$ ${item.price}</h3>
+    </div>`);
+  });
+  console.log(data);
+};
+
+document.addEventListener(
+  "DOMContentLoaded",
+  async function () {
+    await getData();
+  },
+  false
+);
+
+//JQUERY APPROACH
+
+// $(document).ready(() => {
+//   console.log("Ready event");
+//   $.ajax({
+//     type:"GET",
+//     dataType: "json",
+//     crossDomain:true,
+//     url: "https://localhost:7136/items",
+//     headers: {
+//       "Access-Control-Allow-Origin":"*",
+//       "Access-Control-Allow-Headers":"*"
+//     },
+//     // data: {
+//     //   //   zipcode: 97201
+//     // },
+//     success: function (result) {
+//       //   $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
+//       console.log(result);
+//     },
+//       error: function (xhr, status) {
+//       alert("error");
+//   }
+//   });
+// });
 
 // $.ajax({
 //   url: "http://localhost:8079/students/add/",
