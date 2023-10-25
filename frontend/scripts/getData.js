21dd8ad8c8f343a75d9c0bcbe9418319bc08bf6e
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-10-25 12:41:24 +0700

    singleItem page

--- frontend/scripts/getData.js
+++ frontend/scripts/getData.js
@@ -1,22 +1,21 @@
-const url = "https://localhost:44390/api";
 
-const getSingleItem = async (id) => {
-  itemUrl = url + `/products/${id}`;
-  const resp = await fetch(url, {
-    method: "GET",
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(id), // body data type must match "Content-Type" header
-  });
-  const data = await resp.json();
-  console.log(data);
-};
+document.addEventListener(
+  "DOMContentLoaded",
+  async function () {
+    await getData();
+    await getCategories();
+    await getSidebarCategories();
+  },
+  false
+);
+
+
+
+const openItemDetails = (id) =>{
+  window.location.replace(
+    `item.html#${id}`,
+  );
+}
 
 const getData = async () => {
   itemUrl = url + `/products`;
@@ -37,7 +36,7 @@ const getData = async () => {
   data.map((item) => {
     document.querySelector(".productList ").insertAdjacentHTML(
       "beforeend",
-      `<div class ="items">
+      `<div class ="items" onClick =openItemDetails(${item.pid})>
     <img src = "../data/Crawled Images/${item.pid}_1.png"
     />
     <span class="name">${item.pName}</span>
@@ -48,13 +47,8 @@ const getData = async () => {
   });
   console.log(data);
 };
-
-var itemCounter = 0;
-var windowStart = 0;
-var windowEnd = 12;
-var maxItem = 12;
-
-const getCategories = async () => {
+;
+const getSidebarCategories = async () => {
   categoriesUrl = url + "/category";
   const resp = await fetch(categoriesUrl, {
     method: "GET",
@@ -65,18 +59,6 @@ const getCategories = async () => {
     referrerPolicy: "no-referrer",
   });
   const data = await resp.json();
-  // Insert into Navbar
-  data.map((cat, key) => {
-    document
-      .querySelector(".navbar .categories")
-      .insertAdjacentHTML(
-        "beforeend",
-        `<span class="item ${key < maxItem ? "" : "hidden"}">${
-          cat.cName
-        }</span>`
-      );
-  });
-  //Insert into aside
   data.map((cat, key) => {
     document
       .querySelector(".sideBar")
@@ -88,52 +70,5 @@ const getCategories = async () => {
   console.log(data);
 };
 
-document.addEventListener(
-  "DOMContentLoaded",
-  async function () {
-    await getData();
-    await getCategories();
-  },
-  false
-);
 
-//JQUERY APPROACH
 
-// $(document).ready(() => {
-//   console.log("Ready event");
-//   $.ajax({
-//     type:"GET",
-//     dataType: "json",
-//     crossDomain:true,
-//     url: "https://localhost:7136/items",
-//     headers: {
-//       "Access-Control-Allow-Origin":"*",
-//       "Access-Control-Allow-Headers":"*"
-//     },
-//     // data: {
-//     //   //   zipcode: 97201
-//     // },
-//     success: function (result) {
-//       //   $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
-//       console.log(result);
-//     },
-//       error: function (xhr, status) {
-//       alert("error");
-//   }
-//   });
-// });
-
-// $.ajax({
-//   url: "http://localhost:8079/students/add/",
-//   type: "POST",
-//   crossDomain: true,
-//   data: JSON.stringify(somejson),
-//   dataType: "json",
-//   success: function (response) {
-//       var resp = JSON.parse(response)
-//       alert(resp.status);
-//   },
-//   error: function (xhr, status) {
-//       alert("error");
-//   }
-// });
