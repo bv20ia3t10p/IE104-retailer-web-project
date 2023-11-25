a88e8468778da3d4a3854fac8a1f455d5b7d6a24
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-11-25 20:02:23 +0700

    register style

--- frontend/scripts/handleLogin.js
+++ frontend/scripts/handleLogin.js
@@ -5,35 +5,60 @@ var YOUR_CLIENT_ID =
 var YOUR_REDIRECT_URI = "http://127.0.0.1:5500/frontend/login.html";
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#login-form").addEventListener("submit",()=> handleLogin);
+  document
+    .querySelector("#login-form")
+    .addEventListener("submit", () => handleLogin);
   document
     .querySelector("#signinWithGoogle")
-    .addEventListener("click", ()=>signInGoogle);
-  var params = JSON.parse(localStorage.getItem("oauth2-test-params"));
-  console.log(params["access_token"]);
-  if (params && params["access_token"]) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(
-      "GET",
-      "https://www.googleapis.com/oauth2/v3/userinfo?" +
-        "access_token=" +
-        params["access_token"]
-    );
-    xhr.onreadystatechange = function (e) {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.response);
-        serverResponse = JSON.parse(xhr.response);
-        console.log(serverResponse);
-        if (typeof serverResponse.email)
-          document.querySelector("#login-email").value = serverResponse.email;
-        document.querySelector("#login-password").value = serverResponse.email;
-      } else if (xhr.readyState === 4 && xhr.status === 401) {
-        // Token invalid, so prompt for user permission.
-        oauth2SignIn();
-      }
-    };
-    xhr.send(null);
+    .addEventListener("click", () => signInGoogle);
+  try {
+    var params = JSON.parse(localStorage.getItem("oauth2-test-params"));
+    console.log(params["access_token"]);
+    if (params && params["access_token"]) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        "https://www.googleapis.com/oauth2/v3/userinfo?" +
+          "access_token=" +
+          params["access_token"]
+      );
+      xhr.onreadystatechange = function (e) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          console.log(xhr.response);
+          serverResponse = JSON.parse(xhr.response);
+          console.log(serverResponse);
+          if (typeof serverResponse.email)
+            document.querySelector("#login-email").value = serverResponse.email;
+          document.querySelector("#login-password").value =
+            serverResponse.email;
+        } else if (xhr.readyState === 4 && xhr.status === 401) {
+          // Token invalid, so prompt for user permission.
+          oauth2SignIn();
+        }
+      };
+      xhr.send(null);
+    }
+  } catch {
+    console.log("No access token");
   }
+  document.querySelectorAll(".activatePage").forEach((e) =>
+    e.addEventListener("click", () => {
+      const loginPage = document.querySelector(".loginOrRegister .login");
+      const registerPage = document.querySelector(".loginOrRegister .register");
+      loginPage.setAttribute(
+        "class",
+        loginPage.getAttribute("class") === "login activePage"
+          ? "login inactivePage"
+          : "login activePage"
+      );
+      registerPage.setAttribute(
+        "class",
+        registerPage.getAttribute("class") === "register activePage"
+          ? "register inactivePage"
+          : "register activePage"
+      );
+    })
+  );
 });
 
 const signInGoogle = async () => {
@@ -50,15 +75,21 @@ const handleLogin = async (e) => {
     customerPassword: inputData.get("customerPassword"),
   };
   console.log(customer);
-  const resp = await fetch(oath_url + `?GoogleToken=${JSON.parse(localStorage.getItem("oauth2-test-params"))["access_token"]}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify(customer),
-  });
+  const resp = await fetch(
+    oath_url +
+      `?GoogleToken=${
+        JSON.parse(localStorage.getItem("oauth2-test-params"))["access_token"]
+      }`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(customer),
+    }
+  );
   const data = await resp.json();
   console.log(data.token);
 };
