c3bf4bf677760c7c1601122f915aee73e1f22abd
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-11-26 10:20:11 +0700

    localstorage cart

--- frontend/scripts/getData.js
+++ frontend/scripts/getData.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    updateBadge(JSON.parse(localStorage.getItem("cart")).length);
+  }
+  catch (e) {
+    console.log(e);
+  }
   await getData();
   await getCategories();
   await getSidebarCategories();
@@ -25,19 +31,57 @@ const getData = async () => {
   data.map((item) => {
     document.querySelector(".productList.allItems").insertAdjacentHTML(
       "beforeend",
-      `<div class ="items" onClick =openItemDetails(${item.productCardId})>
-    <img src = "../data/Crawled Images/${item.productCardId}_1.png"
+      `<div class ="items" >
+    <img onClick =openItemDetails(${
+      item.productCardId
+    }) src = "../data/Crawled Images/${item.productCardId}_1.png"
     />
     <span class="name">${item.productName}</span>
     <span class="itemSold">${item.productSoldQuantity} sold</span>
     <span class="price">$ ${Math.round(item.productPrice * 1000) / 1000}</span>
-    <span class="button"><img src="icons/addCart.png"/></span>
+    <span class="button" onclick=addToCart(${
+      item.productCardId
+    },1)><img src="icons/addCart.png"/></span>
     </div>`
     );
   });
   productsTemp = data;
   console.log(data);
 };
+
+const addToCart = (id, quantity) => {
+  try {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    console.log(cart);
+    let exist = 0;
+    cart.forEach((item) => {
+      if (item.id === id) {
+        exist = 1;
+        item.quantity += 1;
+        localStorage.setItem("cart", JSON.stringify(cart));
+      }
+    });
+    if (!exist) {
+      let newCart = [...cart, { id, quantity }];
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      updateBadge(newCart.length);
+    }
+  } catch (e) {
+    localStorage.setItem("cart", JSON.stringify([{ id, quantity }]));
+    updateBadge(1);
+  }
+};
+
+const updateBadge = (badgeNumber) => {
+  const badgeClass = document.querySelector(".navbar .action.cart .badge");
+  try {
+    badgeClass.removeChild(badgeClass.lastChild);
+  } catch (e) {
+    console.log(e);
+  }
+  badgeClass.appendChild(document.createTextNode(badgeNumber));
+};
+
 const getPopularProducts = async () => {
   itemUrl =
     "https://localhost:7136/odata/Products?$orderby=ProductSoldQuantity%20desc&top=20";
@@ -53,13 +97,17 @@ const getPopularProducts = async () => {
   data.value.map((item) => {
     document.querySelector(".productList.trending").insertAdjacentHTML(
       "beforeend",
-      `<div class ="items" onClick =openItemDetails(${item.ProductCardId})>
-    <img src = "../data/Crawled Images/${item.ProductCardId}_1.png"
+      `<div class ="items" >
+    <img onClick =openItemDetails(${
+      item.ProductCardId
+    }) src = "../data/Crawled Images/${item.ProductCardId}_1.png"
     />
     <span class="name">${item.ProductName}</span>
     <span class="itemSold">${item.ProductSoldQuantity} sold</span>
     <span class="price">$ ${Math.round(item.ProductPrice * 1000) / 1000}</span>
-    <span class="button"><img src="icons/addCart.png"/></span>
+    <span class="button"  onclick=addToCart(${
+      item.ProductCardId
+    },1)><img src="icons/addCart.png"/></span>
     </div>`
     );
   });
