f86b6b5a121bc8a57aeb1a401a5f35a47fb6a08f
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-11-26 23:29:06 +0700

    item detail style (halfway)

--- frontend/scripts/itemDetail.js
+++ frontend/scripts/itemDetail.js
@@ -1,4 +1,3 @@
-
 function displayHash() {
   var id = window.location.hash.slice(1);
   console.log(id);
@@ -12,12 +11,50 @@ window.addEventListener("hashchange", function () {
 });
 
 window.addEventListener("DOMContentLoaded", function (ev) {
+  try {
+    updateBadge(JSON.parse(localStorage.getItem("cart")).length);
+  } catch (e) {
+    console.log(e);
+  }
   console.log("DOMContentLoaded event");
   displayHash();
 });
 
 currentViewing = 0;
 currentQuantity = 1;
+
+const addToCart = (id, quantity) => {
+  try {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    console.log(cart);
+    let exist = 0;
+    cart.forEach((item) => {
+      if (item.id === id) {
+        exist = 1;
+        item.quantity += quantity;
+        localStorage.setItem("cart", JSON.stringify(cart));
+      }
+    });
+    if (!exist) {
+      let newCart = [...cart, { id, quantity }];
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      updateBadge(newCart.length);
+    }
+  } catch (e) {
+    localStorage.setItem("cart", JSON.stringify([{ id, quantity }]));
+    updateBadge(1);
+  }
+};
+
+const updateBadge = (badgeNumber) => {
+  const badgeClass = document.querySelector(".navbar .action.cart .badge");
+  try {
+    badgeClass.removeChild(badgeClass.lastChild);
+  } catch (e) {
+    console.log(e);
+  }
+  badgeClass.appendChild(document.createTextNode(badgeNumber));
+};
 
 const getSingleItem = async (id) => {
   itemUrl = url + `/Products/${id}`;
@@ -52,16 +89,30 @@ const getSingleItem = async (id) => {
     `
   <div class="ProductInfo">
   <span class="name">${data.productName}</span>
-  <span class="depName">${data.departmentName}</span>
-  <span class ="price">${data.productPrice}</span>
-  <span class ="id">${data.productCardId}</span>
-  <span class="status">${data.productStatus}</span>
+  <span class="depName"><span class="label">Department in charge: </span><span class="data">${
+    data.departmentName
+  }</span></span>
+  <span class ="sold">${data.productSoldQuantity} products sold</span>
+  <span class ="price">$${Math.round(data.productPrice * 1000) / 1000}</span>
+  </div>
+  <div class="AdvancedProductInfo">
+  <h1 class="header">Other product information</h1>
+  <span class ="id"><span class="label">Product Id: </span><span class="data">${
+    data.productCardId
+  }</span></span>
+  <span class="status"><span class="label">Availability Status: </span><span class="data">${
+    data.productStatus ? "Available" : "Not Available (Order for later)"
+  }</span></span>
   </div>`
   );
   document.querySelector(".itemDetailMain").insertAdjacentHTML(
     "beforeend",
     `
-    <div class ="description">${data.productDescription}</div>
+    <div class ="description">
+    <h1 class="header">Product Description</h1>
+    <div class="descriptionDetail">${data.productDescription}</div>
+    <button class="expand">&#x21E9</button>
+    </div>
     <div class="cart">
     <span class="total">${data.productPrice * currentQuantity}</span>
     <span class="quantity">${currentQuantity}</span>
@@ -75,7 +126,39 @@ const getSingleItem = async (id) => {
         Add to cart
       </button></div>`
   );
-
+  document
+    .querySelector(".description .expand")
+    .addEventListener("click", (e) => {
+      let node = document.querySelector(".descriptionDetail");
+      let currentClass = node.getAttribute("class");
+      let expandNode = e.target;
+      try {
+        while (expandNode.firstChild)
+          expandNode.removeChild(expandNode.lastChild);
+      } catch (e) {
+        console.log(e);
+      }
+      expandNode.appendChild(
+        document.createTextNode(
+          currentClass === "descriptionDetail" ? "\u21E7" : "\u21E9"
+        )
+      );
+      node.setAttribute(
+        "class",
+        currentClass === "descriptionDetail"
+          ? "descriptionDetail open"
+          : "descriptionDetail"
+      );
+    });
+  document
+    .querySelector(".itemDetailMain .toCart")
+    .addEventListener("click", () => {
+      addToCart(
+        document.querySelector("body > main > div.AdvancedProductInfo > span.id > span.data")
+          .textContent,
+        currentQuantity
+      );
+    });
   console.log(data);
 };
 
