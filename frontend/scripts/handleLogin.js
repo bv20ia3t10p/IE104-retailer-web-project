e0de7b53b60a0f9c91a9de847a6a930207951fb7
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-12-01 03:04:57 +0700

    1

--- frontend/scripts/handleLogin.js
+++ frontend/scripts/handleLogin.js
@@ -104,7 +104,7 @@ const handleLogin = async (e) => {
   }).catch((e) => alert(e));
   const data = await resp.json();
   var form = document.createElement("form");
-  form.setAttribute("method", "GET"); // Send as a GET request.
+  form.setAttribute("method", "POST"); // Send as a GET request.
   form.setAttribute("action", "index.html");
   document.body.appendChild(form);
   form.submit();
